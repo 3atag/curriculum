d3767b1b6b45971bd83a669091f89b0d11c15f78
Juan <antesdeldomingo@gmail.com>
2020-01-16 17:32:31 -0300

    hasta paso mostrar registros

--- jobs.php
+++ jobs.php
@@ -1,25 +1,9 @@
 <?php
 
-require_once 'vendor/autoload.php';
+use App\Models\{Job,Project};
 
-use App\Models\{Job,Project,Printable};
-
-$job1 = new Job('Programador PHP', 'Este es un trabajo increible');
-
-$job1->visible = true;
-$job1->meses = 16;
-
-
-$job2 = new Job('Programador Javascript', 'Este es un trabajo increible');
-$job2->visible = true;
-$job2->meses = 6;
-
-
-$jobs = [
-    $job1,
-    $job2
-];
-
+// Tremos todos los registros que Elocuent encuentre en el modelo Job
+$jobs = Job::all();
 
 $project1 = new Project ('Intranet','Sistema Intranet para CPHA');
 $project1->visible = true;
@@ -33,27 +17,27 @@ $projects = [
 
 
 
-function imprimirElementos(Printable $elemento)
+function imprimirElementos($elemento)
 {
 
-    if ($elemento->visible == false) {
-        return;
-    }
+    // if ($elemento->visible == false) {
+    //     return;
+    // }
 
     echo '
     <div class="project">
-                <h5>'.$elemento->obtenerTitulo().'</h5>
+                <h5>'.$elemento->title.'</h5>
                 <div class="row">
                     <div class="col-3">
                         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
                       </div>
-                      <div class="col">'.$elemento->obtenerDescripcion().'.</p>
+                      <div class="col">'.$elemento->description.'.</p>
                         <strong>Technologies used:</strong>
                         <span class="badge badge-secondary">PHP</span>
                         <span class="badge badge-secondary">HTML</span>
                         <span class="badge badge-secondary">CSS</span>
                       </div>
-                      <p>'.$elemento->getDuracion().'</p>
+                     
                 </div>
             </div>';
             
